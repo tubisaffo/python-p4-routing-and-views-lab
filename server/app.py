d19586f3ed35ed926a34d1c2f742f93bb41dcb37
tubisaffo <tubisaffo@gmail.com>
2024-06-26 15:08:40 +0300

    routing and views

--- server/app.py
+++ server/app.py
@@ -1,8 +1,33 @@
-#!/usr/bin/env python3
-
 from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:param>')
+def print_string(param):
+    print(f"{param}")
+    return param
+
+@app.route('/count/<int:param>')
+def count(param):
+    numbers = [i for i in range(int(param))]
+    return "\n".join(map(str, numbers)) + '\n' 
+
+@app.route('/math/<int:param1>/<string:operation>/<int:param2>')
+def math(param1, operation, param2):
+    if operation == "+":
+        return str(param1 + param2)
+    elif operation == "-":
+        return str(param1 - param2)
+    elif operation == "*":
+        return str(param1 * param2)
+    elif operation == "div":
+        return str(param1 / param2)
+    elif operation == "%":
+        return str(param1 % param2)
+
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(debug=True)
